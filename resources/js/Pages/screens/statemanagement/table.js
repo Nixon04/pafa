590cb8accdb188fdd8968be41b4bc609b9399f32
nixon04 <nixonsampson04@gmail.com>
2025-06-11 19:13:32 +0100

    Fix Bugs

--- resources/js/Pages/screens/statemanagement/table.js
+++ resources/js/Pages/screens/statemanagement/table.js
@@ -1,65 +1,65 @@
-import { defineStore } from "pinia";
-import { ref, computed } from "vue";
+// import { defineStore } from "pinia";
+// import { ref, computed } from "vue";
 
-export const useTable = defineStore('useTable', () => {
-  const data = ref([]);
+// export const useTable = defineStore('useTable', () => {
+//   const data = ref([]);
 
-  const setData = (newData) => {
-    data.value = newData || [];
-  };
+//   const setData = (newData) => {
+//     data.value = newData || [];
+//   };
 
-  const searchQuery = ref('');
-  const rowsPerPage = ref(5);
-  const currentPage = ref(1);
-  const dropOption = ref([5, 20, 50, 100]);
-  const isLoading = ref(false);
+//   const searchQuery = ref('');
+//   const rowsPerPage = ref(5);
+//   const currentPage = ref(1);
+//   const dropOption = ref([5, 20, 50, 100]);
+//   const isLoading = ref(false);
 
-  const filteredData = computed(() => {
-    if (!data.value) return data.value;
-    if (!searchQuery.value) return data.value;
-    return data.value.filter((item) =>
-      Object.values(item).some((val) =>
-        String(val).toLowerCase().includes(searchQuery.value.toLowerCase())
-      )
-    );
-  });
+//   const filteredData = computed(() => {
+//     if (!data.value) return data.value;
+//     if (!searchQuery.value) return data.value;
+//     return data.value.filter((item) =>
+//       Object.values(item).some((val) =>
+//         String(val).toLowerCase().includes(searchQuery.value.toLowerCase())
+//       )
+//     );
+//   });
 
-  const noResults = computed(() => filteredData.value.length === 0);
+//   const noResults = computed(() => filteredData.value.length === 0);
 
-  const paginatedData = computed(() => {
-    const start = (currentPage.value - 1) * rowsPerPage.value;
-    const end = start + rowsPerPage.value;
-    return filteredData.value.slice(start, end);
-  });
+//   const paginatedData = computed(() => {
+//     const start = (currentPage.value - 1) * rowsPerPage.value;
+//     const end = start + rowsPerPage.value;
+//     return filteredData.value.slice(start, end);
+//   });
 
-  const totalPages = computed(() => {
-    return Math.ceil(filteredData.value.length / rowsPerPage.value);
-  });
+//   const totalPages = computed(() => {
+//     return Math.ceil(filteredData.value.length / rowsPerPage.value);
+//   });
 
-  const nextPage = () => {
-    if (currentPage.value < totalPages.value) currentPage.value++;
-  };
+//   const nextPage = () => {
+//     if (currentPage.value < totalPages.value) currentPage.value++;
+//   };
 
-  const prevPage = () => {
-    if (currentPage.value > 1) currentPage.value--;
-  };
+//   const prevPage = () => {
+//     if (currentPage.value > 1) currentPage.value--;
+//   };
 
-  const changevaluestate = () => {
-    currentPage.value = 1;
-  };
+//   const changevaluestate = () => {
+//     currentPage.value = 1;
+//   };
 
-  return {
-    data,
-    setData,
-    searchQuery,
-    rowsPerPage,
-    currentPage,
-    dropOption,
-    isLoading,
-    noResults,
-    prevPage,
-    changevaluestate,
-    nextPage,
-    paginatedData,
-  };
-});
+//   return {
+//     data,
+//     setData,
+//     searchQuery,
+//     rowsPerPage,
+//     currentPage,
+//     dropOption,
+//     isLoading,
+//     noResults,
+//     prevPage,
+//     changevaluestate,
+//     nextPage,
+//     paginatedData,
+//   };
+// });
--- resources/js/Pages/screens/statemanagement/viewlist.js
+++ resources/js/Pages/screens/statemanagement/viewlist.js
@@ -1,57 +1,57 @@
-import { defineStore } from "pinia";
-import { ref } from 'vue';
-import { toast } from "@steveyuowo/vue-hot-toast";
-import axios from "axios";
+// import { defineStore } from "pinia";
+// import { ref } from 'vue';
+// import { toast } from "@steveyuowo/vue-hot-toast";
+// import axios from "axios";
 
-export const useList = defineStore('useList', () => {
-    const datacall = ref([]);
-    const general = ref('');
-    const paid = ref('');
-    const totalUsers = ref('');
+// export const useList = defineStore('useList', () => {
+//     const datacall = ref([]);
+//     const general = ref('');
+//     const paid = ref('');
+//     const totalUsers = ref('');
 
-    const initFromProps = (first, second, third, forth) => {
+//     const initFromProps = (first, second, third, forth) => {
     
-        datacall.value = first || [];
-        general.value = second || '';
-        paid.value = third || '';
-        totalUsers.value = forth || '';
-        console.log('Paid: ', paid);
-    }
+//         datacall.value = first || [];
+//         general.value = second || '';
+//         paid.value = third || '';
+//         totalUsers.value = forth || '';
+//         console.log('Paid: ', paid);
+//     }
 
-    const isVisible = ref(false);
-    const isLoading = ref(false);
-    const viewlists = ref([]);
+//     const isVisible = ref(false);
+//     const isLoading = ref(false);
+//     const viewlists = ref([]);
 
-    const viewListData = async (item) => {
-        const payload = { itemvalue: item };
-        isVisible.value = true;
-        try {
-            const response = await axios.post('/viewuserslists', payload);
-            if (response.status === 200) {
-                viewlists.value = response.data.message;
-                console.log('View List', viewlists.value);
-            } else {
-                toast.error('Not successful, please contact developer');
-                console.log('Not successful');
-            }
-        } catch (e) {
-            console.log('Error', e);
-        }
-    };
+//     const viewListData = async (item) => {
+//         const payload = { itemvalue: item };
+//         isVisible.value = true;
+//         try {
+//             const response = await axios.post('/viewuserslists', payload);
+//             if (response.status === 200) {
+//                 viewlists.value = response.data.message;
+//                 console.log('View List', viewlists.value);
+//             } else {
+//                 toast.error('Not successful, please contact developer');
+//                 console.log('Not successful');
+//             }
+//         } catch (e) {
+//             console.log('Error', e);
+//         }
+//     };
 
-    const UnClickActive = () => {
-        isVisible.value = false;
-    };
+//     const UnClickActive = () => {
+//         isVisible.value = false;
+//     };
 
-    return {
-        initFromProps,
-        viewListData,
-        UnClickActive,
-        isVisible,
-        viewlists,
-        datacall,
-        general,
-        paid,
-        totalUsers,
-    };
-});
+//     return {
+//         initFromProps,
+//         viewListData,
+//         UnClickActive,
+//         isVisible,
+//         viewlists,
+//         datacall,
+//         general,
+//         paid,
+//         totalUsers,
+//     };
+// });
